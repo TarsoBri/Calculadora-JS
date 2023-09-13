d948f385d95eb7003bfc9ced143e75ce9b2c95dc
Tarso Brietzke Iracet <tarsobrietzkeiracet@gmail.com>
2023-09-12 23:06:49 -0300

    Versão 1.1

--- main.js
+++ main.js
@@ -1,1 +1,85 @@
-console.log('TESTE')
+
+    const display = document.querySelector('#displayInput');
+    const btnEqual = document.querySelector('.equal');
+    const btnPoint = document.querySelector('.point');
+    const btnsNumbers = document.querySelectorAll('.num');
+    const btnsOperators = document.querySelectorAll('.operator');
+
+    var operator = null;
+    var currentValue = '';
+    var previousValue = '';
+    var calculating = false;
+
+
+    
+    
+    function updatyDisplay(){
+        display.value = currentValue;
+    }
+    
+    function insertNumber(event){
+        
+        if(calculating){
+            currentValue = event.target.textContent;
+            calculating = false;
+        } else{
+            currentValue += event.target.textContent;
+        }
+        
+        updatyDisplay();
+    }
+
+    function insertOperador (event){
+        if (currentValue !== "") {
+            if (!calculating) {
+                if (operator !== null){
+                    calculate()
+                };
+                previousValue = currentValue;
+                currentValue = "";
+          }
+          operator = event.target.textContent;
+        }
+    };
+    
+    function insertPoint(){
+        if(currentValue.indexOf('.') === -1){
+            console.log(currentValue.indexOf('.'))
+            currentValue += '.';
+            updatyDisplay();
+        }
+    }
+    
+    function calculate(){
+        var result = null;
+        var previousValueConverted = parseFloat(previousValue);
+        var currentValueConverted = parseFloat(currentValue);
+
+        switch(operator){
+            case '+':
+                result = previousValueConverted + currentValueConverted;
+                break;
+            case '-':
+                result = previousValueConverted - currentValueConverted;
+                break;
+            case '*':
+                result = previousValueConverted * currentValueConverted;
+                break;
+            case '/':
+                result = previousValueConverted / currentValueConverted;
+                break;
+        }
+
+        currentValue = String(result);
+        console.log(currentValue);
+        previousValue = currentValue;
+        calculating = true;
+        updatyDisplay();
+    }
+    
+    
+    btnsNumbers.forEach((btn) => btn.addEventListener('click', insertNumber));
+    btnsOperators.forEach((btn) => btn.addEventListener('click', insertOperador));
+    btnPoint.addEventListener('click', insertPoint);
+    btnEqual.addEventListener('click', calculate);
+    
